dfee8a96a5f870c106b71bb2f94e5863c44d9723
Jean <jeanvictorgithubprojects@gmail.com>
2023-02-16 15:03:18 -0300

    feat(ImportCategory) create load categories and complete execute function to create each categories in csv files

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,22 +1,57 @@
 import fs from 'node:fs'
 import {parse} from 'csv-parse'
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository'
 
+interface IImportCategory {
+  name: string;
+  description: string
+}
 class ImportCategoryUseCase {
-  execute(file: Express.Multer.File): void{
-    const stream = fs.createReadStream(file.path)
+  constructor(private categoriesRepository: ICategoriesRepository){}
+  
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]>{
     
-    const parseFile = parse({
-      quote: false,       
-    }) 
+    return new Promise((resolve, reject) => {
+      const categories:IImportCategory[] = []
+      const stream = fs.createReadStream(file.path)
+      
+      const parseFile = parse({
+        quote: false,       
+      }) 
 
-    // conseguimos pegar os pedaços do arquivo e manda-los para o destino que quisermos
-    stream.pipe(parseFile)
+      // conseguimos pegar os pedaços do arquivo e manda-los para o destino que quisermos
+      stream.pipe(parseFile)
 
-    parseFile.on('data', async (line) => {
-
-      console.log(line)
+      parseFile.on('data', async (line) => {
+        const [name, description] = line
+        categories.push({
+          //Slice para corrigir os parenteses não ocasionais aparecendo
+            name: name.slice(1), 
+            description: description.slice(0, description.length - 1)
+        })
+      }).on("end", () => {
+        resolve(categories)
+      }).on("error", (err) => {
+        reject(err)
+      })
     })
+    
+    
 
+  }
+
+  async execute(file: Express.Multer.File): Promise<void>{
+   const categories = await this.loadCategories(file)
+   categories.map((category) => {
+    const { name, description } = category
+    const existsCategory = this.categoriesRepository.findByName(name)
+    if(!existsCategory){
+      this.categoriesRepository.create({
+        name, 
+        description
+      })
+    }
+   })
   }
 }
 export { ImportCategoryUseCase }
