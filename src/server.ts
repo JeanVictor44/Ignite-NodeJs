a65df0559168b352fb1e87d3cdffa7ef74093b2e
Jean <jeanvictorgithubprojects@gmail.com>
2023-02-23 17:26:21 -0300

    feat: database config added

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,19 @@ import express from 'express'
 import { router } from './routes'
 import swaggerUi from 'swagger-ui-express'
 import swaggerFile from './swagger.json'
+import { AppDataSource } from './database/data-source'
 
-const app = express()
-app.use(express.json())
+AppDataSource.initialize().then(() => {
+  const app = express()
 
-// Endereço da api, função para configurar o server e setup com as configurações em Json do Swagger 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
-app.use(router)
+  app.use(express.json())
 
-app.listen(3333, () => {
-  console.log('Server is running on http://localhost:3333')
+  // Endereço da api, função para configurar o server e setup com as configurações em Json do Swagger 
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
+  app.use(router)
+
+  app.listen(3333, () => {
+    console.log('Server is running on http://localhost:3333')
+  })
+  
 })
- 
